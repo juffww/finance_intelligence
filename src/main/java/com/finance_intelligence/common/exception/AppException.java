3bf05b079d6872b951f37c23cc7bd6057041ca00
juffww <baoho1503@gmail.com>
2026-04-02 19:46:43 +0700

    chore(common): modify AppException, ErrorCode and GlobalExceptionHandler

--- src/main/java/com/finance_intelligence/common/exception/AppException.java
+++ src/main/java/com/finance_intelligence/common/exception/AppException.java
@@ -1,7 +1,15 @@
 package com.finance_intelligence.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class AppException extends RuntimeException {
-    public AppException(String message) {
-        super(message);
+    private ErrorCode errorCode;
+    public AppException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
     }
 }
--- src/main/java/com/finance_intelligence/common/exception/ErrorCode.java
+++ src/main/java/com/finance_intelligence/common/exception/ErrorCode.java
@@ -1,4 +1,17 @@
 package com.finance_intelligence.common.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorCode {
+
+    EMAIL_EXISTED("Email", "Email is exited", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED("User", "User is not existed", HttpStatus.BAD_REQUEST);
+
+    private String field;
+    private String message;
+    HttpStatus httpStatus;
 }
