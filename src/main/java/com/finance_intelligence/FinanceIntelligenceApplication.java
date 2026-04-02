f53c1ee6339949b3af82888b6402eacdbab3f549
juffww <baoho1503@gmail.com>
2026-04-02 18:27:25 +0700

    initial commit

---
+++ src/main/java/com/finance_intelligence/FinanceIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.finance_intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance_intelligence/FinanceIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance_intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
